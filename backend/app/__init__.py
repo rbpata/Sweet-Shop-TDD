aa39ced14a4852482e6116e3ac12acfde5166b45
Ram <imrbp.2k04@gmail.com>
2025-07-27 11:11:03 +0530

    refactor: restructure app factory in __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from .config import Config 
+from .config import Config
 
 db = SQLAlchemy()
 jwt = JWTManager()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -16,16 +17,21 @@ def create_app(test_config=None):
         app.config.from_object(Config)
 
     # Enable CORS
-    CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5173"], supports_credentials=True)
+    CORS(
+        app,
+        origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        supports_credentials=True,
+    )
 
     db.init_app(app)
     jwt.init_app(app)
-    
+
     from app.routes.auth import auth_bp
     from app.routes.sweets import sweet_bp
-    
+    from app.routes.inventory import inventory_bp
+
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(sweet_bp, url_prefix="/api/sweets")
+    app.register_blueprint(inventory_bp, url_prefix="/api/inventory")
 
-    
     return app
